394eca05127d2620d63393d61dfd3711275c64e1
Renato Groffe <renato.groffe@yahoo.com.br>
2022-05-29 21:05:42 -0300

    .NET 6 + ASP.NET Core + Minimal APIs + Swagger/OpenApi + Conversão de temperaturas (Fahrenheit para as escalas Celsius e Kelvin)
    .NET 6 + ASP.NET Core + Minimal APIs + Swagger/OpenApi + Conversão de temperaturas (Fahrenheit para as escalas Celsius e Kelvin)

---
+++ APITemperaturas/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.OpenApi.Models;
+using APITemperaturas.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "APITemperaturas",
+            Description = "API de conversão de temperaturas implementada com .NET 7 + Minimal APIs", 
+            Version = "v1",
+            Contact = new OpenApiContact()
+            {
+                Name = "Renato Groffe",
+                Url = new Uri("https://github.com/renatogroffe"),
+            },
+            License = new OpenApiLicense()
+            {
+                Name = "MIT",
+                Url = new Uri("http://opensource.org/licenses/MIT"),
+            }
+        });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "APITemperaturas v1");
+});
+
+app.MapGet("/ConversorTemperaturas/Fahrenheit/{temperaturaFahrenheit}",
+    (double temperaturaFahrenheit) =>
+    {
+        if (temperaturaFahrenheit < -459.67)
+        {
+            app.Logger.LogError($"Valor de temperatura em Fahrenheit inválido: {temperaturaFahrenheit}");
+            return Results.BadRequest();
+        }
+        var result = new Temperatura(temperaturaFahrenheit);
+        app.Logger.LogInformation($"{result.Fahrenheit} graus Fahrenheit = " +
+            $"{result.Celsius} graus Celsius = " +
+            $"{result.Kelvin} graus Kelvin");
+        return Results.Ok(result);
+    }).Produces<Temperatura>()
+      .Produces(StatusCodes.Status400BadRequest);
+
+app.Run();
